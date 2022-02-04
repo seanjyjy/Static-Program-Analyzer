19e72926c88e16c60982dae9d467fb7259948696
Sean <e0406739@u.nus.edu>
2022-02-04 22:48:04 +0800

    Added UsesP evaluator

---
+++ Team41/Code41/src/spa/src/QPS/Evaluator/UsesPEvaluator.cpp
@@ -0,0 +1,122 @@
+#include "Evaluator.h"
+
+Table* evaluateUsesIdentifierIdentifier(PKB);
+Table* evaluateUsesIdentifierSynonym(PKB);
+Table* evaluateUsesIdentifierWildCard(PKB);
+Table* evaluateUsesSynonymIdentifier(PKB);
+Table* evaluateSynonymSynonym(PKB);
+Table* evaluateSynonymWildCard(PKB);
+
+Table* Evaluator::evaluate(string clause, PKB* pkb) {
+    // Get first argument
+    // Get second argument
+
+    if (firstArgument.isIdentifier() && secondArgument.isIdentifer()) {
+        return evaluateUsesIdentifierIdentifier(pkb, firstArgument, secondArgument);
+    }
+
+    if (firstArgument.isIdentifier() && secondArgument.isSynonym()) {
+        return evaluateUsesIdentifierSynonym(pkb, firstArgument, secondArgument);
+    }
+
+    if (firstArgument.isIdentifier() && secondArgument.isWildCard()) {
+        return evaluateUsesIdentifierWildCard(pkb, firstArgument);
+    }
+
+    if (firstArgument.isSynonym() && secondArgument.isIdentifer()) {
+        return evaluateUsesSynonymIdentifier(pkb, firstArgument, secondArgument);
+    }
+
+    if (firstArgument.isSynonym() && secondArgument.isSynonym()) {
+        return evaluateSynonymSynonym(pkb, firstArgument, secondArgument);
+    }
+
+    if (firstArgument.isSynonym() && secondArgument.isWildCard()) {
+        return evaluateSynonymWildCard(pkb, firstArgument);
+    }
+
+    return new FalseTable();
+}
+
+Table* evaluateUsesIdentifierIdentifier(PKB* pkb, firstArgument, secondArgument) {
+    bool isUsesP = pkb->isUsesP(firstArgument->getIdentifier(), secondArgument->getIdentifier());
+
+    if (isUsesP) {
+        return new TrueTable();
+    }
+
+    return new FalseTable();
+}
+
+Table* evaluateUsesIdentifierSynonym(PKB* pkb, firstArgument, secondArgument) {
+    unordered_set<string> setOfVariables = pkb->getUsesByProc(firstArgument->getIdentifier());
+
+    string column = secondArgument->getSynonym();
+    Header header = Header({column});
+    Table* result = new PQLTable(header);
+
+    for (auto& variable : setOfVariables) {
+        Row* row = new Row(column, variable);
+        result->addRow(row);
+    }
+
+    return result;
+}
+
+Table* evaluateUsesIdentifierWildCard(PKB* pkb, firstArgument) {
+    unordered_set<string> setOfVariables = pkb->getUsesByProc(firstArgument->getIdentifier());
+
+    if (setOfVariables.empty()) {
+        return new FalseTable();
+    }
+
+    return new TrueTable();
+}
+
+Table* evaluateUsesSynonymIdentifier(PKB* pkb, firstArgument, secondArgument) {
+    unordered_set<string> setOfProcedures = pkb->getUsesPByVar(secondArgument->getIdentifier());
+
+    string column = firstArgument->getSynonym();
+    Header header = Header({column});
+    Table* result = new PQLTable(header);
+
+    for (auto& procedure : setOfProcedures) {
+        Row* row = new Row(column, procedure);
+        result->addRow(row);
+    }
+
+    return result;
+}
+
+Table* evaluateSynonymSynonym(PKB* pkb, firstArgument, secondArgument) {
+    vector<pair<string, string>> listOfProcedureToVariable =  pkb->getAllUsesP();
+
+    string firstColumn = firstArgument->getSynonym();
+    string secondColumn = secondArgument->getSynonym();
+    Header header = Header({firstColumn, secondColumn});
+    Table* result = new PQLTable(header);
+
+    for (auto& procedureToVariable : listOfProcedureToVariable) {
+        Row* row = new Row();
+        row->addEntry(firstColumn, procedureToVariable.first);
+        row->addEntry(secondColumn, procedureToVariable.second);
+        result->addRow(row);
+    }
+
+    return result;
+}
+
+Table* evaluateSynonymWildCard(PKB* pkb, firstArgument) {
+    vector<pair<string, string>> listOfProcedureToVariable =  pkb->getAllUsesP();
+
+    string column = firstArgument->getSynonym();
+    Header header = Header({column});
+    Table* result = new PQLTable(header);
+
+    for (auto& procedureToVariable : listOfProcedureToVariable) {
+        Row* row = new Row(column, procedureToVariable.first);
+        result->addRow(row);
+    }
+
+    return result;
+}
