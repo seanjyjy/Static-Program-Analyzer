4edb71bc41bf49e2ed631218c126d7de36076d05
Hardy Shein <>
2022-02-06 01:42:55 +0800

    Added additional variables for QueryObject

--- Team41/Code41/src/spa/src/QPS/QueryObject.h
+++ Team41/Code41/src/spa/src/QPS/QueryObject.h
@@ -11,6 +11,7 @@ class QueryObject {
 public:
     vector<QueryDeclaration> &declarations;
     vector<QueryClause> &clauses; // Actual clauses parser affects
+    QueryDeclaration &selectSynonym;
     bool isQueryValid;
-    QueryObject(vector<QueryDeclaration> &declarations, vector<QueryClause> &clauses, bool isQueryValid);
+    QueryObject(vector<QueryDeclaration> &declarations, vector<QueryClause> &clauses, QueryDeclaration &selectSynonym, bool isQueryValid);
 };
