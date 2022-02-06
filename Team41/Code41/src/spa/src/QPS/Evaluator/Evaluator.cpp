096559d7668241335688d6cadcff48a29a611179
Sean <e0406739@u.nus.edu>
2022-02-06 16:29:55 +0800

    remove unused evaluator file

--- Team41/Code41/src/spa/src/QPS/Evaluator/Evaluator.cpp
+++
@@ -1,8 +0,0 @@
-#include "Evaluator.h"
-#include "QPS/Table/PQLTable.h"
-
-Table* Evaluator::evaluate(QueryClause clause, PKB* pkb) {
-    PQLTable* table;
-    table = nullptr;
-    return table;
-}
