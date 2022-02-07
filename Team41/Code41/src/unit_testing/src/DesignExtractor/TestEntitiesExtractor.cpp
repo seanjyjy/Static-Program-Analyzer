fe8c2f13caab41950d4e4c6fe43e696cbeeb0ec1
Min Suk <minschoi97@gmail.com>
2022-02-07 20:16:55 +0800

    Design Extractor Unit Tests

---
+++ Team41/Code41/src/unit_testing/src/DesignExtractor/TestEntitiesExtractor.cpp
@@ -0,0 +1,52 @@
+#include "catch.hpp"
+
+#include "DesignExtractor/EntitiesExtractor.h"
+#include "SimpleParser/Parser.h"
+
+using namespace std;
+
+TEST_CASE("DesignExtractor: Register Entities") {
+    // todo create ast without Parser
+    string s = "procedure main {\n"
+               "\tread x;\n"
+               "\ty = z + 1;\n"
+               "\twhile (b != 1) {\n"
+               "\t\tc = 2;\n"
+               "\t\twhile (c > 1) {\n"
+               "\t\t\tx = y + c;\n"
+               "\t\t}\n"
+               "\t\tprint a;\n"
+               "\t}\n"
+               "\tif (f > 1) then {\n"
+               "\t\tprint q;\n"
+               "\t} else {\n"
+               "\t\th = i + j;\n"
+               "\t}\n"
+               "}";
+    Parser p = Parser{s};
+    TNode* ast = p.parse();
+    EntitiesExtractor ee = EntitiesExtractor(ast);
+    ee.extractEntities();
+
+    SECTION("Statement Nums") {
+        unordered_set<string> expectedStmt = {"1", "2", "3", "4", "5",
+                                              "6", "7", "8", "9", "10"};
+        REQUIRE(ee.getNodeToStmtNumMap().size() == expectedStmt.size());
+    }
+
+    SECTION("Procedures") {
+        unordered_set<string> expectedProcedures = {"main"};
+        REQUIRE(ee.getProcSet() == expectedProcedures);
+    }
+
+    SECTION("Variable") {
+        unordered_set<string> expectedVarNames = {"x", "y", "z", "b", "c", "a",
+                                                  "f", "q", "h", "i", "j"};
+        REQUIRE(ee.getVarSet() == expectedVarNames);
+    }
+
+    SECTION("Constants") {
+        unordered_set<string> expectedConstVals = {"1", "2"};
+        REQUIRE(ee.getConstSet() == expectedConstVals);
+    }
+}
