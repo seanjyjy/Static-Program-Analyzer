bbbe13d94cc9d0a1c4c4342ebab891b3b1ee3fc0
Kendrick Ang <kendrick.wh@outlook.com>
2022-02-09 14:51:41 +0800

    chore: add comments to AstBuilder.h

--- Team41/Code41/src/spa/src/Common/AstBuilder.h
+++ Team41/Code41/src/spa/src/Common/AstBuilder.h
@@ -6,8 +6,10 @@
 
 using namespace std;
 
+// Represents a parsed xml tag.
 class XmlTag {
 private:
+    // accepted names in xml tag, e.g <program> ... </program>
     const string T_PROGRAM = "program";
     const string T_PROCEDURE = "procedure";
     const string T_STMTLIST = "stmts";
@@ -34,67 +36,94 @@ private:
     const string T_DIV = "div";
     const string T_MOD = "mod";
 
+    // returns an empty program ast node.
     TNode *convertProgram();
 
+    // reads procedure name "name" from data and returns a partially populated procedure ast node.
     TNode *convertProcedure();
 
+    // returns an empty statement list ast node.
     TNode *convertStmtList();
 
+    // returns an empty read node.
     TNode *convertRead();
 
+    // returns an empty print ast node.
     TNode *convertPrint();
 
+    // returns an empty call ast node.
     TNode *convertCall();
 
+    // returns an empty while ast node.
     TNode *convertWhile();
 
+    // returns an empty if ast node.
     TNode *convertIf();
 
+    // returns an empty assign ast node.
     TNode *convertAssign();
 
+    // returns an empty not ast node.
     TNode *convertNot();
 
+    // returns an empty and ast node.
     TNode *convertAnd();
 
+    // returns an empty or ast node.
     TNode *convertOr();
 
+    // returns an empty > ast node.
     TNode *convertGt();
 
+    // returns an empty >= ast node.
     TNode *convertGe();
 
+    // returns an empty < ast node.
     TNode *convertLt();
 
+    // returns an empty <= ast node.
     TNode *convertLe();
 
+    // returns an empty == ast node.
     TNode *convertEq();
 
+    // returns an empty != ast node.
     TNode *convertNe();
 
+    // reads variable name "name" from data and returns a variable ast node.
     TNode *convertVar();
 
+    // reads constant value "val" from data and returns an integer ast node.
     TNode *convertConst();
 
+    // returns an empty + ast node.
     TNode *convertPlus();
 
+    // returns an empty - ast node.
     TNode *convertMinus();
 
+    // returns an empty * ast node.
     TNode *convertTimes();
 
+    // returns an empty / ast node.
     TNode *convertDiv();
 
+    // returns an empty % ast node.
     TNode *convertMod();
 
+    // checks that all given keys are present in the data map, throws otherwise.
     void ensureKeys(const vector<string> &keys);
 
 public:
-    string type;
-    unordered_map<string, string> data;
-    bool isCloseTag;
+    string type; // same as types given above
+    unordered_map<string, string> data; // e.g <var name=hi></var>, key: name, value: "hi"
+    bool isCloseTag; // e.g </var>
 
     XmlTag();
 
     XmlTag(string type, unordered_map<string, string> data, bool isCloseTag);
 
+    // converts the xml tag to its corresponding AST node.
     TNode *convert();
 };
 
@@ -104,34 +133,46 @@ private:
     const char OPEN_TAG = '<';
     const char CLOSE_TAG = '>';
 
-    string xml;
-    int idx;
-    char currToken;
-    stack<pair<TNode *, bool>> stk; // node/token, isOpeningTag
+    string xml; // the input string in "xml" format
+    int idx; // index position in the input string
+    char currToken; // current char mirroring index position
+    stack<pair<TNode *, bool>> stk; // { node, isOpeningTag }
     stack<XmlTag> tagStk; // mirrors stk, meant to ensure correctness
 
+    // moves idx forward by one and updates the current char.
     void advance();
 
+    // eats '<', an optional '/', a name (alphanumeric), optional key=value pairs, then '>', in that order.
     XmlTag eatXmlTag();
 
+    // consumes the current char until a non-whitespace (by isspace(c)) is met.
     void eatWhitespace();
 
+    // consumes '<'.
     void eatOpenTag();
 
+    // consumes '>'.
     void eatCloseTag();
 
+    // consumes '='.
     void eatAssign();
 
+    // returns true if '/' is the current token, false otherwise. If true, '/' is consumed.
     bool eatSlashIfExists();
 
+    // consumes an alphanumeric contiguous string.
     string eatName();
 
+    // consumes multiple patterns of <ws>key<ws>=<ws>val<ws>, until a closing tag is met. <ws> = whitespace
     unordered_map<string, string> eatData();
 
+    // returns true if the current token is EOF_TOKEN, false otherwise.
     bool isEof();
 
+    // returns true if the current token is '>', false otherwise.
     bool isCloseTag();
 
+    // returns true if the current token is '<', false otherwise.
     bool isOpenTag();
 
 public:
@@ -140,5 +181,6 @@ public:
 
     AstBuilder(string xml);
 
+    // returns an ast node based on the input xml; only returns a correct representation on first call.
     TNode *build();
 };
