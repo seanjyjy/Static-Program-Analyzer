a8c3f13a70c1885b44839cd2e49aec9e1c0ea43c
Kendrick Ang <kendrick.wh@outlook.com>
2022-02-09 13:34:47 +0800

    refactor: make TreeUtils isEqual() more readable

--- Team41/Code41/src/spa/src/Common/TreeUtils.cpp
+++ Team41/Code41/src/spa/src/Common/TreeUtils.cpp
@@ -1,15 +1,22 @@
 #include "TreeUtils.h"
 
 bool TreeUtils::isEqual(TNode *n1, TNode *n2) {
-    // base case: if children are different sizes, not equal
-    if (n1->getChildren().size() != n2->getChildren().size()) return false;
-    // otherwise first compare type and underlying token value (if any)
-    bool isEq = true;
-    if (n1->getVal() == nullptr && n2->getVal() == nullptr) {
-        isEq = isEq && (n1->getType() == n2->getType());
+    // base case: if nodes are different types or children are different sizes, not equal
+    if (n1->getType() != n2->getType() || n1->getChildren().size() != n2->getChildren().size()) return false;
+
+    // compare underlying token value (if any)
+    bool isEq;
+    Token *val1 = n1->getVal();
+    Token *val2 = n2->getVal();
+    if (val1 == nullptr && val2 == nullptr) {
+        isEq = true;
+    } else if (val1 != nullptr && val2 != nullptr){
+        isEq = val1->getVal() == val2->getVal();
     } else {
-        isEq = isEq && (n1->getType() == n2->getType()) && (n1->getVal()->getVal() == n2->getVal()->getVal());
+        // one nullptr, other is not nullptr
+        isEq = false;
     }
+
     // then compare children equality
     for (int i = 0; i < n1->getChildren().size(); i++) {
         isEq = isEq && isEqual(n1->getChildren()[i], n2->getChildren()[i]);
