6ab96785faad1306e2820e292bc83fd426c79c72
Min Suk <minschoi97@gmail.com>
2022-02-17 23:41:29 +0800

    move simple xml files to unit_testing/simple

--- Team41/Code41/src/unit_testing/src/SimpleParser/TestParserUtils.cpp
+++ Team41/Code41/src/unit_testing/src/SimpleParser/TestParserUtils.cpp
@@ -138,7 +138,7 @@ TNode *TestParserUtils::parseXml(const string& xml) {
 string TestParserUtils::readFile(string folder, string filename) {
     filesystem::path cwd = filesystem::current_path();
     cwd = cwd.parent_path().parent_path().parent_path();
-    vector<string> paths = {"src", "unit_testing", "src", "Common", "AstBuilder", "input", folder, filename};
+    vector<string> paths = {"src", "unit_testing", "simple", folder, filename};
     for (const string& p: paths) {
         cwd /= p;
     }
