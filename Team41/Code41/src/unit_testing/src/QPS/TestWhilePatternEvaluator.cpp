b0dac0408c1c525737df4045a36306d1e8720fb0
Jin Hao <limjinhao@gmail.com>
2022-03-09 20:44:59 +0800

    test: QE pattern unit tests

---
+++ Team41/Code41/src/unit_testing/src/QPS/TestWhilePatternEvaluator.cpp
@@ -0,0 +1,89 @@
+#include "catch.hpp"
+#include "PKB/PKBManager.h"
+#include "PKB/Tables/WhilePatternTable.h"
+#include "QPS/Evaluator/WhilePatternEvaluator.h"
+#include "QPS/ClauseVariable.h"
+#include "./TableTestUtils.h"
+#include "../Common/AstBuilder/TestAstBuilderUtils.h"
+
+TEST_CASE("Evaluator: While pattern evaluator") {
+    PKBManager *pkbManager = new PKBManager();
+    string lines[] = {"1", "2", "3", "4"};
+    string WHILE_SYN_LBL = "whiles";
+    string VAR_SYN_LBL = "v1";
+    string vars[] = {"var0", "var1", "var2"};
+
+    QueryDeclaration whileSyn(QueryDeclaration::WHILE, WHILE_SYN_LBL);
+
+    ClauseVariable identifierV0(ClauseVariable::identifier, vars[0], QueryDeclaration::VARIABLE);
+    ClauseVariable identifierV1(ClauseVariable::identifier, vars[1], QueryDeclaration::VARIABLE);
+    ClauseVariable variableSyn(ClauseVariable::synonym, VAR_SYN_LBL, QueryDeclaration::VARIABLE);
+    ClauseVariable wildcard(ClauseVariable::wildcard, "_", QueryDeclaration::VARIABLE);
+
+    PatternVariable patternWildCard(PatternVariable::wildcard, nullptr);
+    PatternVariable patternFP(PatternVariable::fullpattern, nullptr);
+    PatternVariable patternSP(PatternVariable::subpattern, nullptr);
+
+    pkbManager->registerWhilePattern(lines[0], vars[0]); // 0: v0
+    pkbManager->registerWhilePattern(lines[1], vars[1]); // 1: v1
+    pkbManager->registerWhilePattern(lines[2], vars[2]); // 2: v2
+    pkbManager->registerWhilePattern(lines[3], vars[0]); // 3: v0
+    pkbManager->registerWhilePattern(lines[3], vars[1]); // 3: v1
+
+    SECTION("Synonym WildCards Pair") {
+        PatternClause patternClause1(whileSyn, variableSyn, vector<PatternVariable>({patternWildCard, patternWildCard}));
+        Table *table1 = WhilePatternEvaluator::evaluate(patternClause1, pkbManager);
+        vector<string> expectedColumns1{WHILE_SYN_LBL, VAR_SYN_LBL};
+        vector<vector<string>> expectedRows1{{lines[0], vars[0]},
+                                             {lines[1], vars[1]},
+                                             {lines[2], vars[2]},
+                                             {lines[3], vars[0]},
+                                             {lines[3], vars[1]}};
+
+        REQUIRE(TableTestUtils::checkTableMatches(table1, expectedColumns1, expectedRows1));
+    }
+
+    SECTION("Identifier WildCards Pair") {
+        PatternClause patternClause1(whileSyn, identifierV0, vector<PatternVariable>({patternWildCard, patternWildCard}));
+        Table *table1 = WhilePatternEvaluator::evaluate(patternClause1, pkbManager);
+        vector<string> expectedColumns1{WHILE_SYN_LBL};
+        vector<vector<string>> expectedRows1{{lines[0]},
+                                             {lines[3]}};
+        REQUIRE(TableTestUtils::checkTableMatches(table1, expectedColumns1, expectedRows1));
+
+        PatternClause patternClause2(whileSyn, identifierV1, vector<PatternVariable>({patternWildCard, patternWildCard}));
+        Table *table2 = WhilePatternEvaluator::evaluate(patternClause2, pkbManager);
+        vector<string> expectedColumns2{WHILE_SYN_LBL};
+        vector<vector<string>> expectedRows2{{lines[1]},
+                                             {lines[3]}};
+        REQUIRE(TableTestUtils::checkTableMatches(table2, expectedColumns2, expectedRows2));
+    }
+
+    SECTION("WildCard WildCards Pair") {
+        PatternClause patternClause1(whileSyn, wildcard, vector<PatternVariable>({patternWildCard, patternWildCard}));
+        Table *table1 = WhilePatternEvaluator::evaluate(patternClause1, pkbManager);
+        vector<string> expectedColumns1{WHILE_SYN_LBL};
+        vector<vector<string>> expectedRows1{{lines[0]},
+                                             {lines[1]},
+                                             {lines[2]},
+                                             {lines[3]}};
+        REQUIRE(TableTestUtils::checkTableMatches(table1, expectedColumns1, expectedRows1));
+    }
+
+    SECTION("Semantically & Syntactically Invalid") {
+        ClauseVariable procSyn(ClauseVariable::synonym, "proc", QueryDeclaration::PROCEDURE);
+        PatternClause patternClause2(whileSyn, procSyn, vector<PatternVariable>({patternWildCard}));
+        REQUIRE_THROWS(WhilePatternEvaluator::evaluate(patternClause2, pkbManager));
+
+        ClauseVariable readSyn(ClauseVariable::synonym, "read", QueryDeclaration::READ);
+        PatternClause patternClause3(whileSyn, readSyn, vector<PatternVariable>({patternWildCard}));
+        REQUIRE_THROWS(WhilePatternEvaluator::evaluate(patternClause3, pkbManager));
+
+        PatternClause patternClause4(whileSyn, variableSyn, vector<PatternVariable>({patternFP}));
+        REQUIRE_THROWS(WhilePatternEvaluator::evaluate(patternClause4, pkbManager));
+
+        PatternClause patternClause5(whileSyn, variableSyn, vector<PatternVariable>({patternSP}));
+    }
+
+    delete pkbManager;
+}
