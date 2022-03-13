a3b2cf409257bf59a710eab14a4124eb5f9bc341
Jin Hao <limjinhao@gmail.com>
2022-03-14 00:26:47 +0800

    feat: add pkb optimizer costs

--- Team41/Code41/src/spa/src/QPS/Optimizer/PKBAdapter.cpp
+++ Team41/Code41/src/spa/src/QPS/Optimizer/PKBAdapter.cpp
@@ -4,8 +4,30 @@ PKBAdapter::PKBAdapter(PKBManager *pkb): pkb(pkb) {
 }
 
 long long PKBAdapter::getRowCount(const ClauseVariable &synonym) {
-    // TODO stub
-    return 0;
+    switch (synonym.getDesignEntityType()) {
+        case QueryDeclaration::STMT:
+            return pkb->getStatementCount();
+        case QueryDeclaration::READ:
+            return pkb->getReadCount();
+        case QueryDeclaration::PRINT:
+            return pkb->getPrintCount();
+        case QueryDeclaration::CALL:
+            return pkb->getCallCount();
+        case QueryDeclaration::WHILE:
+            return pkb->getWhileCount();
+        case QueryDeclaration::IF:
+            return pkb->getIfCount();
+        case QueryDeclaration::ASSIGN:
+            return pkb->getAssignCount();
+        case QueryDeclaration::VARIABLE:
+            return pkb->getVariableCount();
+        case QueryDeclaration::CONSTANT:
+            return pkb->getConstantCount();
+        case QueryDeclaration::PROCEDURE:
+            return pkb->getProcedureCount();
+        default:
+            return pkb->getStatementCount();
+    }
 }
 
 long long PKBAdapter::getRowCount(const vector<ClauseVariable> &synonyms) {
