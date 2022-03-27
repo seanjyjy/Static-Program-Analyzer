94f183b30598adf43a91128383cbdecb568c724f
Kendrick Ang <kendrick.wh@outlook.com>
2022-03-27 14:12:20 +0800

    tests: more test cases for clausedepgraph and queryoptimizer

--- Team41/Code41/src/unit_testing/src/QPS/Optimizer/TestClauseDepGraph.cpp
+++ Team41/Code41/src/unit_testing/src/QPS/Optimizer/TestClauseDepGraph.cpp
@@ -19,10 +19,11 @@ TEST_CASE("Clause Dependency Graph: basic") {
     REQUIRE(clauseGrps[1].size() == 1); // Modifies(s, "x")
 }
 
-TEST_CASE("Clause Dependency Graph: week 7 lecture example") {
+TEST_CASE("Clause Dependency Graph: complex") {
     ClauseDepGraph g;
     string pql = "assign a1, a2, a3; stmt s1, s2, s3; variable v1, v2, v3;\n"
-                 "Select <s1, s2, s3> such that Uses(s3, v1) and Modifies(s3, \"x\") and Follows(s1, s2) and Parent(s3, s1) and Uses(s2, v1) pattern a1(v1, _) pattern a2(v1, _) with v1.stmt# = 3";
+                 "Select <s1, s2, s3> such that Uses(s3, v1) and Modifies(s3, \"x\") and Follows(s1, s2) "
+                 "and Parent(s3, s1) and Uses(s2, v1) pattern a1(v1, _) pattern a2(v1, _) with v1.stmt# = 3";
     QueryParser qp = QueryParser{pql};
     unique_ptr<QueryObject> queryObject(qp.parse());
     for (SuperClause *cl: queryObject->getSuperClauses()) {
@@ -32,6 +33,31 @@ TEST_CASE("Clause Dependency Graph: week 7 lecture example") {
     sort(clauseGrps.begin(), clauseGrps.end(), [](auto &a, auto &b) {return a.size() < b.size();});
     REQUIRE(clauseGrps.size() == 1);
     REQUIRE(clauseGrps[0].size() == 8);
+}
+
+TEST_CASE("Clause Dependency Graph: week 7 lecture example") {
+    ClauseDepGraph g;
+    string pql = "assign a1, a2, a3; stmt s1, s2, s3; variable v1, v2, v3;\n"
+                   "Select <s1, s2, s3> such that Uses(s3, v1) and Modifies(s3, \"x\") and Follows(s1, s2) "
+                   "and Parent(s3, s1) and Uses(s2, v1) such that Uses(5, \"y\") and Follows(3, 4) "
+                   "pattern a1(v2, _\"x+y\"_) such that Affects(a1, a2) with a2.stmt# = 20 "
+                   "such that Modifies(a3, v3) pattern a3(\"z\", _)";
+    QueryParser qp = QueryParser{pql};
+    unique_ptr<QueryObject> queryObject(qp.parse());
+    for (SuperClause *cl: queryObject->getSuperClauses()) {
+        g.registerClause(cl);
+    }
+    vector<vector<SuperClause*>> clauseGrps = g.split();
+    sort(clauseGrps.begin(), clauseGrps.end(), [](auto &a, auto &b) {return a.size() < b.size();});
+    // Follows(3,4), Uses(5,"y")
+    // pattern a3("z",_) Modifies(a3, v3)
+    // with a2.stmt# = 20 Affects(a1, a2) pattern a1(v2, _"x+y"_)
+    // Modifies(s3,"x") Parent(s3,s1) Follows(s1,s2) Uses(s3,v1) Uses(s2,v1)
+    REQUIRE(clauseGrps.size() == 4);
+    REQUIRE(clauseGrps[0].size() == 2);
+    REQUIRE(clauseGrps[1].size() == 2);
+    REQUIRE(clauseGrps[2].size() == 3);
+    REQUIRE(clauseGrps[3].size() == 5);
 }
 
 TEST_CASE("Clause Dependency Graph: should not remove duplicate clauses") {
--- Team41/Code41/src/unit_testing/src/QPS/Optimizer/TestQueryOptimizer.cpp
+++ Team41/Code41/src/unit_testing/src/QPS/Optimizer/TestQueryOptimizer.cpp
@@ -69,6 +69,15 @@ TEST_CASE("Query Optimizer: integration test1") {
         TestOptimizerUtils::ensureOQOIsCorrect(query, pkbManager);
     }
 
+    SECTION("Query 5 (extra)") {
+        string query = "assign a1, a2, a3; stmt s1, s2, s3; variable v1, v2, v3;\n"
+                       "Select <s1, s2, s3> such that Uses(s3, v1) and Modifies(s3, \"x\") and Follows(s1, s2) "
+                       "and Parent(s3, s1) and Uses(s2, v1) such that Uses(5, \"y\") and Follows(3, 4) "
+                       "pattern a1(v2, _\"x+y\"_) such that Affects(a1, a2) with a2.stmt# = 20 "
+                       "such that Modifies(a3, v3) pattern a3(\"z\", _)";
+        TestOptimizerUtils::ensureOQOIsCorrect(query, pkbManager);
+    }
+
     delete ast;
 }
 
