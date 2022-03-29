011c502823b6284229272103f6edcf3a4c9b0e3d
Kendrick Ang <kendrick.wh@outlook.com>
2022-03-30 01:05:57 +0800

    test: add reducible clause group test - select BOOLEAN case

--- Team41/Code41/src/unit_testing/src/QPS/Optimizer/TestQueryOptimizerOptimizations.cpp
+++ Team41/Code41/src/unit_testing/src/QPS/Optimizer/TestQueryOptimizerOptimizations.cpp
@@ -80,7 +80,7 @@ TEST_CASE("Query Optimizer: Simplifiable groups 3") {
 
 // https://github.com/nus-cs3203/21s2-cp-spa-team-41/issues/287
 // this will probably break easily if we change our sort logic. beware!
-TEST_CASE("QueryOptimizer: Simplifiable Groups sort order") {
+TEST_CASE("QueryOptimizer: Simplifiable Groups sort order - select synonyms") {
     string query = "procedure p; stmt s1, s2, s3, s4, s5, s6, s7; variable v1, v2, v5, v6, v6, v7;\n"
                    "Select p such that Uses(s1, v1) and Modifies(s2, v1) and Uses(s3, v2) and Modifies(s4, v2) and Uses(s5, v5) and Uses(s6, v6) and Uses(s7, v7) and Uses(p, \"x\") and Modifies(p, \"y\")";
     // parse the query and setup objects
@@ -113,6 +113,46 @@ TEST_CASE("QueryOptimizer: Simplifiable Groups sort order") {
     REQUIRE(oqo.isLastOfGroup());
     REQUIRE_NOTHROW(oqo.popClause());
     REQUIRE(oqo.currGroupCanSimplify() == false);
+    REQUIRE(oqo.currGroupSize() == 2);
+    REQUIRE_NOTHROW(oqo.popClause());
+    REQUIRE(oqo.isLastOfGroup());
+    REQUIRE_NOTHROW(oqo.popClause());
+    REQUIRE(oqo.empty());
+}
+
+TEST_CASE("QueryOptimizer: Simplifiable Groups sort order - select BOOLEAN") {
+    string query = "procedure p; stmt s1, s2, s3, s4, s5, s6, s7; variable v1, v2, v5, v6, v6, v7;\n"
+                   "Select BOOLEAN such that Uses(s1, v1) and Modifies(s2, v1) and Uses(s3, v2) and Modifies(s4, v2) and Uses(s5, v5) and Uses(s6, v6) and Uses(s7, v7) and Uses(p, \"x\") and Modifies(p, \"y\")";
+    // parse the query and setup objects
+    PKBManager pkbManager;
+    QueryParser qp = QueryParser{query};
+    QueryObject *qo(qp.parse());
+    OptimizedQueryObject oqo = QueryOptimizer::create()
+            .enableAllOptimizations(&pkbManager)
+            .optimize(qo);
+    REQUIRE(oqo.currGroupCanSimplify() == true);
+    REQUIRE(oqo.isLastOfGroup());
+    REQUIRE(oqo.currGroupSize() == 1);
+    REQUIRE_NOTHROW(oqo.popClause());
+    REQUIRE(oqo.currGroupCanSimplify() == true);
+    REQUIRE(oqo.isLastOfGroup());
+    REQUIRE(oqo.currGroupSize() == 1);
+    REQUIRE_NOTHROW(oqo.popClause());
+    REQUIRE(oqo.currGroupCanSimplify() == true);
+    REQUIRE(oqo.isLastOfGroup());
+    REQUIRE(oqo.currGroupSize() == 1);
+    REQUIRE_NOTHROW(oqo.popClause());
+    REQUIRE(oqo.currGroupCanSimplify() == true);
+    REQUIRE(oqo.currGroupSize() == 2);
+    REQUIRE_NOTHROW(oqo.popClause());
+    REQUIRE(oqo.isLastOfGroup());
+    REQUIRE_NOTHROW(oqo.popClause());
+    REQUIRE(oqo.currGroupCanSimplify() == true);
+    REQUIRE(oqo.currGroupSize() == 2);
+    REQUIRE_NOTHROW(oqo.popClause());
+    REQUIRE(oqo.isLastOfGroup());
+    REQUIRE_NOTHROW(oqo.popClause());
+    REQUIRE(oqo.currGroupCanSimplify() == true);
     REQUIRE(oqo.currGroupSize() == 2);
     REQUIRE_NOTHROW(oqo.popClause());
     REQUIRE(oqo.isLastOfGroup());
